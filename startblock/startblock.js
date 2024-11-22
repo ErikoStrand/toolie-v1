7663b964d8ef220f0f3d047fdb787afd336a27ab
BergarnJ <hoogleonard@gmail.com>
2024-11-22 13:44:44 +0100

    Tiny changes

--- startblock/startblock.js
+++ startblock/startblock.js
@@ -1,13 +1,30 @@
 const notearea = document.getElementById("notearea");
 
+function expand() {}
+
 function addNewTextarea() {
   let li = document.createElement("li");
+  let div = document.createElement("div");
+  let input = document.createElement("input");
   let textarea = document.createElement("textarea");
-  let button = document.createElement("button");
+  let del_button = document.createElement("button");
+  let exp_button = document.createElement("button");
+
   li.setAttribute("id", "note");
-  button.setAttribute("onclick", "this.parentElement.remove()");
-  button.textContent = "Radera anteckning";
-  li.appendChild(button);
+  input.type = "text";
+  input.setAttribute("placeholder", "Anteckning");
+  del_button.setAttribute(
+    "onclick",
+    "this.parentElement.parentElement.remove()"
+  );
+  del_button.textContent = "Radera anteckning";
+  exp_button.setAttribute("onclick", expand());
+  exp_button.textContent = "Expandera";
+
+  div.appendChild(input);
+  div.appendChild(exp_button);
+  div.appendChild(del_button);
+  li.appendChild(div);
   li.appendChild(textarea);
   notearea.appendChild(li);
 }
