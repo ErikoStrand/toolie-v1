6ac68f1a8e6f5131e416970ac21c1566f407595a
Matti Leskinen <matti.leskinen@learnet.se>
2024-12-06 13:54:38 +0100

    You can now save groups to a JSON file and attempted to do the custom window stuff in main. Will refine this alongside doing some CSS next week.

--- main.js
+++ main.js
@@ -4,10 +4,13 @@ const path = require("path");
 
 function createMainWindow() {
   const mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 145,
+    height: 330,
     maximizable: false,
     alwaysOnTop: true,
+    transparent: true,
+    titleBarStyle: "hidden",
+    resizable: false,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
@@ -17,12 +20,57 @@ function createMainWindow() {
 
   mainWindow.loadFile("index.html");
 }
+function createNamnSlumpare(path) {
+  const newWindow = new BrowserWindow({
+    width: 600,
+    height: 800,
+    transparent: true,
+    alwaysOnTop: true,
+    maximizable: false,
+    titleBarStyle: "hidden",
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
 
+  newWindow.loadFile(path);
+}
+
+function createStartBlock(path) {
+  const newWindow = new BrowserWindow({
+    width: 600,
+    height: 800,
+    transparent: true,
+    alwaysOnTop: true,
+    maximizable: false,
+    titleBarStyle: "hidden",
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  newWindow.loadFile(path);
+}
+
+function createGroupie(path) {
+  const newWindow = new BrowserWindow({
+    width: 720,
+    height: 1280,
+    transparent: true,
+    alwaysOnTop: true,
+    maximizable: false,
+    titleBarStyle: "hidden",
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  newWindow.loadFile(path);
+}
 function createWindow(path) {
-  const mainWindow = BrowserWindow.getAllWindows()[0]; // Get main window
-  if (mainWindow) {
-    mainWindow.hide(); // Hide instead of close to preserve app state
-  }
   const newWindow = new BrowserWindow({
     width: 400,
     height: 300,
@@ -35,12 +83,6 @@ function createWindow(path) {
   });
 
   newWindow.loadFile(path);
-
-  newWindow.on("closed", () => {
-    if (mainWindow) {
-      mainWindow.show();
-    }
-  });
 }
 
 // Handle the getPath request
@@ -54,6 +96,18 @@ app.whenReady().then(() => {
   ipcMain.on("newWindow", (event, arg) => {
     console.log(arg);
     createWindow(arg);
+  });
+  ipcMain.on("namnSlumpare", (event, arg) => {
+    console.log(arg);
+    createNamnSlumpare(arg);
+  });
+  ipcMain.on("startblock", (event, arg) => {
+    console.log(arg);
+    createStartBlock(arg);
+  });
+  ipcMain.on("groupie", (event, arg) => {
+    console.log(arg);
+    createGroupie(arg);
   });
 });
 
