9b7e2e851c957f0b12badf7961a9fc5b4c59ab44
Erik Strandberg <erikstrandberg153@gmail.com>
2024-12-18 15:36:29 +0100

    what, why doesnt toolie-eclectron start?!

--- main.js
+++ main.js
@@ -4,11 +4,6 @@ const { exec } = require("child_process");
 const path = require("path");
 const { start } = require("repl");
 
-// Build/Installer stuff ----------------------------
-if (require("electron-squirrel-startup")) {
-  app.quit();
-}
-
 function createMainWindow(which) {
   //Första main Window med alla knappar
   const location = loadLocation(which);
