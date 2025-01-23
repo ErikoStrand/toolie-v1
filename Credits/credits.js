7ed4fcac5d7433776111b237e279b698ad0ead7a
Sten Kaal <sten.kaal@learnet.se>
2025-01-23 11:30:00 +0100

    works, but with alot of commits it doesnt.

--- Credits/credits.js
+++ Credits/credits.js
@@ -1,16 +1,19 @@
 const fs = require("fs");
 
-var textArea = document.getElementById("RepoText");
-var repoButton = document.getElementById("getRepoButton");
-
 async function getCommits() {
   fs.readFile("resources/data/commit_history.json", "utf-8", (err, data) => {
     if (err) {
       console.error("Failed to load json", err);
       return;
     }
-    console.log(data);
+    parsedData = JSON.parse(data);
+    let textContainer = document.getElementById("text-container");
+    for (let i = parsedData.length - 1; i >= 0; i--) {
+      console.log(`${parsedData[i].author}: ${parsedData[i].message}`);
+      let p = document.createElement("p");
+
+      p.textContent = `${parsedData[i].author}: ${parsedData[i].message}`;
+      textContainer.appendChild(p);
+    }
   });
 }
-
-repoButton.addEventListener("click", getCommits);
